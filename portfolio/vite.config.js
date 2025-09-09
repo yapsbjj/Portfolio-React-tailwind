dc286e7409be437fdffba2002cf0ca0a87c25ba7
unknown <ya.ramdane01@gmail.com>
2025-09-09 22:16:49 +0200

    remove backend

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
+// Recréer __dirname en ESM
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
